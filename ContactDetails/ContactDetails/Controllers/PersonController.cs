03f7bf11ef816578aaab893355d4fc56950f0b25
Ahsan <ahsan1037@gmail.com>
2018-03-16 20:13:29 +0600

    Project Set up

---
+++ ContactDetails/ContactDetails/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using ContactDetails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactDetails.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(Contact model)
+        {
+            return View();
+        }
+    }
+}
